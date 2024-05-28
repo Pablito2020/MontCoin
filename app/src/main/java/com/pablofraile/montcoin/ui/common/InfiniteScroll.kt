287ff946b94a1e46c3314834689053470f3453d6
Pablo Fraile Alonso <pablo@pablofraile.net>
2024-05-28 08:28:52 +0200

    feat: better infinite scroll component

---
+++ app/src/main/java/com/pablofraile/montcoin/ui/common/InfiniteScroll.kt
@@ -0,0 +1,60 @@
+package com.pablofraile.montcoin.ui.common
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.pulltorefresh.PullToRefreshContainer
+import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
+@Composable
+fun <T> InfiniteScroll(
+    elements: List<T>,
+    itemRender: @Composable (T, Modifier) -> Unit,
+    onRefresh: suspend () -> Unit,
+    loadMoreItems: () -> Unit,
+    refreshedMessage: String,
+    modifier: Modifier = Modifier,
+    snackbarHostState: SnackbarHostState
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val state = rememberPullToRefreshState()
+    if (state.isRefreshing) {
+        LaunchedEffect(true) {
+            onRefresh()
+            val showSnackBar =
+                coroutineScope.launch { snackbarHostState.showSnackbar(refreshedMessage) }
+            state.endRefresh()
+            showSnackBar.join()
+        }
+    }
+    Box(modifier = modifier.nestedScroll(state.nestedScrollConnection)) {
+        LazyColumn(modifier = Modifier.padding(top=2.dp, start=2.dp).fillMaxHeight()) {
+            items(elements) { element ->
+                Box(modifier = Modifier.animateItemPlacement()) {
+                    itemRender(element, modifier)
+                }
+                val index = elements.indexOf(element)
+                if (index == elements.size - 1)
+                    loadMoreItems()
+            }
+        }
+        PullToRefreshContainer(
+            modifier = Modifier.align(Alignment.TopCenter),
+            state = state,
+        )
+    }
+}
--- app/src/main/java/com/pablofraile/montcoin/ui/operations/OperationsScreen.kt
+++ app/src/main/java/com/pablofraile/montcoin/ui/operations/OperationsScreen.kt
@@ -1,15 +1,15 @@
 package com.pablofraile.montcoin.ui.operations
 
 import android.widget.Toast
-import androidx.compose.foundation.layout.Box
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Search
@@ -23,22 +23,18 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
-import androidx.compose.material3.pulltorefresh.PullToRefreshContainer
-import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.pablofraile.montcoin.model.Operation
 import com.pablofraile.montcoin.model.Operations
-import kotlinx.coroutines.launch
+import com.pablofraile.montcoin.ui.common.InfiniteScroll
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -85,60 +81,39 @@ fun OperationsScreen(
             )
         }
     ) { innerPadding ->
-        OperationsContent(
-            operations = operations,
-            modifier = Modifier.padding(innerPadding),
+        InfiniteScroll(
+            elements = operations,
+            itemRender = { operation, modifier ->
+                OperationItem(
+                    operation = operation,
+                    modifier = modifier
+                )
+            },
             onRefresh = onRefresh,
+            loadMoreItems = loadMoreItems,
+            refreshedMessage = "Loaded last Operations!",
             snackbarHostState = snackbarHostState,
-            loadMoreItems = loadMoreItems
-        )
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun OperationsContent(
-    operations: Operations,
-    modifier: Modifier = Modifier,
-    onRefresh: suspend () -> Unit,
-    loadMoreItems: () -> Unit = {},
-    snackbarHostState: SnackbarHostState
-) {
-    val coroutineScope = rememberCoroutineScope()
-    val state = rememberPullToRefreshState()
-    if (state.isRefreshing) {
-        LaunchedEffect(true) {
-            onRefresh()
-            val showSnackBar =
-                coroutineScope.launch { snackbarHostState.showSnackbar("Loaded last Operations!") }
-            state.endRefresh()
-            showSnackBar.join()
-        }
-    }
-    Box(modifier = modifier.nestedScroll(state.nestedScrollConnection)) {
-        LazyColumn(modifier = modifier) {
-            items(operations) { transaction ->
-                OperationItem(transaction, modifier = modifier)
-                val index = operations.indexOf(transaction)
-                if (index == operations.size - 1)
-                    loadMoreItems()
-            }
-        }
-        PullToRefreshContainer(
-            modifier = Modifier.align(Alignment.TopCenter),
-            state = state,
+            modifier = Modifier.padding(innerPadding)
         )
     }
 }
 
 @Composable
-fun OperationItem(operation: Operation, modifier: Modifier) {
+fun OperationItem(operation: Operation, modifier: Modifier = Modifier) {
     val amountValue = operation.amount.toInt()
     val amountColor = if (amountValue >= 0) Color.Green else Color.Red
-    Card {
+    Card(modifier = Modifier.padding(start = 2.dp, end = 2.dp, top = 2.dp, bottom = 2.dp)) {
         Row(
             modifier = Modifier
-                .padding(16.dp)
+                .padding(start = 32.dp, end = 24.dp, top = 8.dp, bottom = 8.dp)
+                .animateContentSize(
+                    animationSpec = tween(
+                        durationMillis = 200,
+                        delayMillis = 0,
+                        easing = LinearEasing,
+                    ),
+                )
+                .clip(MaterialTheme.shapes.medium)
                 .fillMaxWidth(),
             verticalAlignment = Alignment.CenterVertically
         ) {
